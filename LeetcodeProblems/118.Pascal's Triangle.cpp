b075744ae9a3ce2d9df6edb24ce0143f9db5a146
KusumChaudhary <kusumchaudhary97720@gmail.com>
2025-10-22 13:00:10 +0530

    Create 118.Pascal's Triangle.cpp
    Created .Pascal's Triangle.cpp

---
+++ LeetcodeProblems/118.Pascal's Triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result;
+        vector<int> row;
+
+        for (int i = 0; i < numRows; i++) {
+            row.push_back(1); 
+
+            for (int j = i - 1; j > 0; j--) {
+                row[j] = row[j] + row[j - 1];
+            }
+
+            result.push_back(row); 
+        }
+
+        return result;
+    }
+};
